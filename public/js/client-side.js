47dc97c76125217b4d936db72a73ad695bd3fb48
dayadam <dayadamuga@gmail.com>
2019-12-04 13:29:43 -0500

    removed more linkedin stuff

--- public/js/client-side.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $(".search").click(function() {
-    $.ajax({
-      url: "http://localhost:8080/api/job-search",
-      method: "GET"
-    }).then(x => console.log(x));
-  });
-});
