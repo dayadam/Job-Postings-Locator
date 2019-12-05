e5b82c9d73758a1616f233d6ddac31214240eae5
ugo070 <ugo070@yahoo.com>
2019-12-05 13:00:47 -0500

    submit form

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,35 +1,32 @@
 const db = require("../models");
 const passport = require("../config/passport");
 const axios = require("axios");
-
-const joobleKey = process.env.JOOBLE_API;
-const googleKey = process.env.GOOGLE_API;
+const joobleKey = process.env.JOOBLE_API_KEY;
+const googleKey = process.env.GOOGLE_API_KEY;
 
 module.exports = function(app) {
+  //get jobs
+  app.get("/api/search", function(req, res) {
+    const city = "Atlanta";
+    axios
+      .post(`https://jooble.org/api/${joobleKey}`, {
+        keywords: "Front End Developer",
+        location: "Atlanta",
+        radius: "50",
 
-
-    //get jobs
-    app.get("/api/search", function(req, res) {
-        const city = "Atlanta";
-        axios
-            .post(`https://jooble.org/api/${joobleKey}`, {
-                keywords: "Front End Developer",
-                location: "Atlanta",
-                radius: "50",
-
-                page: "1",
-                searchMode: "1"
-            })
-            .then(function(response) {
-                return companyToLoc(response.data.jobs, city);
-            })
-            .then(function(jobs) {
-                res.json(jobs);
-            })
-            .catch(function(err) {
-                res.status(500).json({ err: err.message });
-            });
-    });
+        page: "1",
+        searchMode: "1"
+      })
+      .then(function(response) {
+        return companyToLoc(response.data.jobs, city);
+      })
+      .then(function(jobs) {
+        res.json(jobs);
+      })
+      .catch(function(err) {
+        res.status(500).json({ err: err.message });
+      });
+  });
   app.post("/api/job-search", function(req, res) {
     const URL = `https://jooble.org/api/${joobleKey}`;
 
@@ -65,8 +62,6 @@ module.exports = function(app) {
       });
   });
 
-
-
   // user login post authenticates using the "local" strat in the passport.js
   app.post("/api/login", passport.authenticate("local"), function(req, res) {
     //sends pac the route to redirect to if the user is "logged in"
@@ -91,36 +86,30 @@ module.exports = function(app) {
     res.redirect("/");
   });
 
+  async function companyToLoc(jobs, city) {
+    const locationRequests = [];
 
-    async function companyToLoc(jobs, city) {
-        const locationRequests = [];
+    for (let i = 0; i < jobs.length; i++) {
+      const companyName = encodeURI(jobs[i].company);
+      const urlString = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${companyName},${city}&inputtype=textquery&fields=formatted_address,geometry&key=${googleKey}`;
 
-        for (let i = 0; i < jobs.length; i++) {
-            const companyName = encodeURI(jobs[i].company);
-            const urlString = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${companyName},${city}&inputtype=textquery&fields=formatted_address,geometry&key=${googleKey}`;
+      const locationReq = axios.get(urlString).then(function(response) {
+        let location = {};
 
-            const locationReq = axios.get(urlString).then(function(response) {
-                let location = {};
-
-                if (response.data.status !== "ZERO_RESULTS") {
-                    location.address = response.data.candidates[0].formatted_address;
-                    location.lat = response.data.candidates[0].geometry.location.lat;
-                    location.lng = response.data.candidates[0].geometry.location.lng;
-                }
-
-                return location;
-            });
-            locationRequests.push(locationReq);
+        if (response.data.status !== "ZERO_RESULTS") {
+          location.address = response.data.candidates[0].formatted_address;
+          location.lat = response.data.candidates[0].geometry.location.lat;
+          location.lng = response.data.candidates[0].geometry.location.lng;
         }
-        const locations = await Promise.all(locationRequests);
-        for (let i = 0; i < locations.length; i++) {
-            jobs[i].location = locations[i];
-        }
-        return jobs;
 
+        return location;
+      });
+      locationRequests.push(locationReq);
     }
-    // )
-    //console.log();
+    const locations = await Promise.all(locationRequests);
+    for (let i = 0; i < locations.length; i++) {
+      jobs[i].location = locations[i];
+    }
     return jobs;
   }
 };
