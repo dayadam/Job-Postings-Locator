1a00fa197204db7dc096a89b361e20e0d7687176
Daniel Cornnell <daniel.cornnell@gmail.com>
2019-12-06 12:08:43 -0500

    added test cases for timeouts

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,14 +1,16 @@
 const db = require("../models");
 const passport = require("../config/passport");
 const axios = require("axios");
-
+//set these in .env file
 const joobleKey = process.env.JOOBLE_API_KEY;
 const googleKey = process.env.GOOGLE_API_KEY;
 
 module.exports = function(app) {
     //get jobs with location data
+
     app.put("/api/search", function(req, res) {
         const searchLoc = req.body.location;
+        const jobs = {};
         axios
             .post(`https://jooble.org/api/${joobleKey}`, {
                 keywords: req.body.keywords,
@@ -19,6 +21,8 @@ module.exports = function(app) {
                 searchMode: "1"
             })
             .then(function(response) {
+                const jobs = response.data.jobs;
+
                 return companyToLoc(
                     res,
                     response.data.jobs,
@@ -30,13 +34,15 @@ module.exports = function(app) {
                 res.json(jobs);
             })
             .catch(function(err) {
-                res.status(500).json({ err: err.message });
+                console.log("timed out sending cached data");
+                getCacheData(res, searchLoc, req.body.keywords, jobs);
             });
     });
-    //get jobs without location data
+    //get jobs without location data this link isnt really need the /api/search will
+    // send this info if it cant find it in db or api call. this is for testing
     app.get("/api/job-search", function(req, res) {
         const URL = `https://jooble.org/api/${joobleKey}`;
-        console.log(URL);
+
         axios
             .post(URL, {
                 keywords: "manager",
@@ -67,12 +73,6 @@ module.exports = function(app) {
                 res.json(err);
             });
     });
-    //test route
-    app.get("/api/test", function(req, res) {
-        db.Search.findOne({ where: { id: 1 } }).then(function(response) {
-            res.json(JSON.parse(response.jobs));
-        });
-    });
 
     // user login post authenticates using the "local" strat in the passport.js
     app.post("/api/login", passport.authenticate("local"), function(req, res) {
@@ -97,16 +97,21 @@ module.exports = function(app) {
         req.logout();
         res.redirect("/");
     });
-
+    // main bulk of the app, its async because we need to make multiple calls to google places
+    // we need them all to return before we move on.
     async function companyToLoc(res, jobs, searchLoc, keywords) {
         const locationRequests = [];
-        let timer = false;
-
+        // this whill check if we sent data.
+        let dataSent = false;
+        // if the call is taking too long we can set a time to send the database data.
         setTimeout(function() {
-            if (timer === false) {
+            //if the data has been found we dont need to grab the db data so we check
+            //if the data has already beed grabbed and do nothing if it has.
+            if (dataSent === false) {
                 getCacheData(res, searchLoc, keywords, jobs);
             }
-        }, 20000);
+        }, 30000);
+        //takes the incoming jobs and makes a call to the google places api thehn adds the log / lat to the object
         for (let i = 0; i < jobs.length; i++) {
             const companyName = encodeURI(jobs[i].company);
             const urlString = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${companyName},${searchLoc}&inputtype=textquery&fields=formatted_address,geometry&key=${googleKey}`;
@@ -122,13 +127,17 @@ module.exports = function(app) {
 
                 return location;
             });
+            //adds each promise to an array
             locationRequests.push(locationReq);
         }
+        //waits till all the promises come in then adds the jobs
         const locations = await Promise.all(locationRequests);
         for (let i = 0; i < locations.length; i++) {
             jobs[i].location = locations[i];
         }
-        timer = true;
+        //tells the timeout function we have already sent data.
+        dataSent = true;
+        // adds the found data to the database
         createCacheData(keywords, searchLoc, jobs);
         return jobs;
     }
@@ -142,18 +151,24 @@ module.exports = function(app) {
             if (response) {
                 res.json(JSON.parse(response.jobs));
             } else {
+                console.log("no datafound in db sending jobs without long and lat");
                 res.json(jobs);
             }
         });
     }
 
     function createCacheData(keywords, searchLoc, jobs) {
-        console.log(jobs);
-
         db.Search.create({
-            search: keywords.toLowerCase(),
-            location: searchLoc,
-            jobs: JSON.stringify(jobs)
-        });
+                search: keywords.toLowerCase(),
+                location: searchLoc.toLowerCase(),
+                jobs: JSON.stringify(jobs)
+            })
+            .then(function(user) {
+                console.log("success", user.toJSON());
+            })
+            .catch(function(err) {
+                // print the error details
+                console.log(err);
+            });
     }
 };
